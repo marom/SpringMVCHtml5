477a49dd8111856455769d5cb629c672f6bb7fd4
MaroM <maro.muszynski@gmail.com>
2016-05-10 07:53:15 +0200

    add Quartz and Cron task scheduling

---
+++ src/main/java/spring/mvc/quartz/CronQuartzTask.java
@@ -0,0 +1,11 @@
+package spring.mvc.quartz;
+
+/**
+ * Created by maro on 10/05/16.
+ */
+public class CronQuartzTask {
+
+    public void cronTaskMethod() {
+        //System.out.println("Cron time: " + Calendar.getInstance().getTime());
+    }
+}
---
+++ src/main/java/spring/mvc/quartz/QuartzTask.java
@@ -0,0 +1,11 @@
+package spring.mvc.quartz;
+
+/**
+ * Created by maro on 10/05/16.
+ */
+public class QuartzTask {
+
+    public void simpleTaskMethod() {
+        //System.out.println("Simple Quartz time " + Calendar.getInstance().getTime());
+    }
+}
